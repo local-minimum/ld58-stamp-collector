b97dd0156c4d7b2e6683757f0122824aa6a4a5f0
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-10-04 15:13:17 +0200

    Stamp collecting ball

--- scripts/ball_controller.gd
+++ scripts/ball_controller.gd
@@ -2,6 +2,8 @@ extends RigidBody3D
 class_name BallController
 
 @export var rotation_speed: float = 10
+@export var stamp_versions: Array[Node3D]
+@export var stamp_colliders: Array[CollisionShape3D]
 
 var start_positon: Vector3
 var _acceleration: Vector3
@@ -15,6 +17,25 @@ func _ready() -> void:
 
     if __SignalBus.on_level_completed.connect(_handle_complete_level) != OK:
         push_error("Failed to connect level completed")
+
+    if __SignalBus.on_collect_stamp.connect(_handle_collect_stamp) != OK:
+        push_error("Failed to connect collect stamp")
+
+    _sync(0)
+
+var _collected_stamps: Array[CollectableStamp]
+
+func _sync(stamps: int) -> void:
+    for idx: int in range(stamp_versions.size()):
+        stamp_versions[idx].visible = idx == stamps
+
+    for idx: int in range(stamp_colliders.size()):
+        stamp_colliders[idx].set_deferred("disabled", idx > stamps)
+
+func _handle_collect_stamp(stamp: CollectableStamp) -> void:
+    if !_collected_stamps.has(stamp):
+        _collected_stamps.append(stamp)
+        _sync(_collected_stamps.size())
 
 func _input(event: InputEvent) -> void:
     var x: float = event.get_action_strength("roll_west") - event.get_action_strength("roll_east")
@@ -42,6 +63,8 @@ func kill() -> void:
     _acceleration = Vector3.ZERO
     global_position = start_positon
     _started = false
+    _collected_stamps.clear()
+    _sync(0)
 
 func _handle_complete_level() -> void:
     _level_completed = true
