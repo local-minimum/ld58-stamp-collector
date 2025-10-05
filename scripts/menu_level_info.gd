35ebd8d05570717eb7c2def6439450019b49cfc3
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-10-05 13:39:38 +0200

    level info in menu

---
+++ scripts/menu_level_info.gd
@@ -0,0 +1,50 @@
+extends Control
+
+@export var container: Control
+@export var level_name: Label
+@export var level_deaths: Label
+@export var level_record: Label
+
+var showing: int = -1
+
+func _ready() -> void:
+    if __SignalBus.on_menu_show_level_info.connect(_handle_show_info) != OK:
+        push_error("Failed to connect show level info")
+
+    if __SignalBus.on_menu_hide_level_info.connect(_handle_hide_info) != OK:
+        push_error("Failed to connect hide level info")
+
+    _sync()
+
+func _handle_show_info(idx: int) -> void:
+    showing = idx
+    print_debug("[Level Info] Level %s" % idx)
+    _sync()
+
+func _handle_hide_info(idx: int) -> void:
+    if showing == idx:
+        showing = -1
+
+        _sync()
+
+func _sync() -> void:
+    if showing == -1:
+        container.visible = false
+        return
+
+    var level_id: String = __LevelsManager.level_ids[showing] if __LevelsManager.level_ids.size() > showing else ""
+    if level_id.is_empty():
+        container.visible = false
+        return
+
+    level_name.text = __LevelsManager.level_names[showing] if __LevelsManager.level_names.size() > showing else "???"
+
+    level_deaths.text = "%s" % __GlobalState.get_deaths(level_id)
+
+    var record: int = __GlobalState.get_best_time(level_id)
+    if record >= 0:
+        level_record.text = "%.3fs" % (record / 1000.0)
+    else:
+        level_record.text = "---"
+
+    container.visible = true
