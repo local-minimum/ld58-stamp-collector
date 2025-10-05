b794aa45de3552e92c27775b8f0137af3a5ac721
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-10-05 12:07:53 +0200

    Menu selection

--- scripts/menu_hole.gd
+++ scripts/menu_hole.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var _load_level_idx: int
 @export var _lid: Node3D
 @export var _lid_collider: CollisionShape3D
+@export var _level_label: Label
 
 var open: bool = false
 
@@ -12,6 +13,7 @@ func _ready() -> void:
 
     _lid.visible = !open
     _lid_collider.set_deferred("disabled", open)
+    _level_label.text = "%s" % (_load_level_idx + 1)
 
 func _on_hole_trigger_body_entered(body:Node3D) -> void:
     if body is BallController:
